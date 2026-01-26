243537800553610b3f91eae140afbd2a510c36e3
creatif-sam <samuel.creatiftech@gmail.com>
2026-01-26 14:30:30 +0100

    26/01-2-recent 3

--- app/auth/confirm/route.ts
+++ app/auth/confirm/route.ts
@@ -14,7 +14,7 @@ export async function GET(request: Request) {
   }
 
   const supabase = createServerSupabaseClient({
-    cookies: cookies()
+    cookies:await cookies()
   })
 
   await supabase.auth.verifyOtp({
